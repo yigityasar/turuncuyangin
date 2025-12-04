3e715d06273bc88852fe1fbe457c3553be4a08c9
Yiğit Yaşar <yigityasar.dev@gmail.com>
2025-12-04 17:56:46 +0300

    Kodlar eklendi

---
+++ _next/static/chunks/app/page-90a1bb204222bc5b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8185:function(e,n,s){Promise.resolve().then(s.t.bind(s,6964,23)),Promise.resolve().then(s.t.bind(s,4724,23))}},function(e){e.O(0,[724,964,971,864,744],function(){return e(e.s=8185)}),_N_E=e.O()}]);
